db5ffaa625dae754b442af0a798aa3581fe8cd79
Matthew J Preisendorfer <mpreisendorfer@gmail.com>
2024-12-01 15:46:00 -0500

    Create server.py

---
+++ webserver/server.py
@@ -0,0 +1,9 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def start_web_server():
+    logger.info("Starting Web Server...")
+    #app.run(host='127.0.0.1', debug=True,port="5000")
+    return
+    
